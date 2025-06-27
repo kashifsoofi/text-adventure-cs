8a5488f672b6de5c5ad239ca2798ba65a06bb465
Kashif Soofi <kashif.soofi@onceforall.com>
2025-06-27 16:34:14 +0100

    Project and test setup

---
+++ TextAdventure.Tests.Integration/TextAdventureTests.cs
@@ -0,0 +1,22 @@
+﻿using Shouldly;
+
+namespace TextAdventure.Tests.Integration;
+
+public class TextAdventureTests
+{
+    [Fact]
+    public void TestMain()
+    {
+        // Arrange
+        using var sw = new StringWriter();
+        Console.SetOut(sw);
+        
+        var expected = File.ReadAllText("baseline.txt");
+
+        // Act
+        Program.Main([]);
+        
+        // Assert
+        sw.ToString().ShouldBe(expected);
+    }
+}
---
+++ TextAdventure/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace TextAdventure;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Little Cave Adventure.");
+        Console.WriteLine("It is very dark in here.");
+        Console.WriteLine("");
+        Console.WriteLine("Bye!");
+    }
+}

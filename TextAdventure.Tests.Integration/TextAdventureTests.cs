c30dcf4c4c0ecfd9e106eb43e7d569e2c69f9dbf
Kashif Soofi <kashif.soofi@onceforall.com>
2025-06-27 17:19:15 +0100

    Implement main loop

--- TextAdventure.Tests.Integration/TextAdventureTests.cs
+++ TextAdventure.Tests.Integration/TextAdventureTests.cs
@@ -10,13 +10,37 @@ public class TextAdventureTests
         // Arrange
         using var sw = new StringWriter();
         Console.SetOut(sw);
+
+        var input = File.ReadAllText("TestScript.txt").Replace("\r\n", "\n");
+        using var sr = new StringReader(input);
+        Console.SetIn(sr);
         
-        var expected = File.ReadAllText("baseline.txt");
+        var expected = File.ReadAllText("BaseLine1.txt");
 
         // Act
         Program.Main([]);
         
         // Assert
-        sw.ToString().ShouldBe(expected);
+        var output = GetNormalisedOutput(sw, input);
+        output.ShouldBe(expected);
+    }
+
+    private string GetNormalisedOutput(StringWriter sw, string input)
+    {
+        var output = sw.ToString();
+        var outputLines = output.Replace("--> ", "--> \n") .Split('\n');;
+        var inputLines = input.Split("\n");
+
+        var j = 0;
+        for (var i = 0; i < outputLines.Length; i++)
+        {
+            if (outputLines[i].StartsWith("--> "))
+            {
+                outputLines[i] = $"--> {inputLines[j]}";
+                j++;
+            }
+        }
+
+        return string.Join('\n', outputLines);
     }
 }
